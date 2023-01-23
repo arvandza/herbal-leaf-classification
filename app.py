6b11c5c94769f93215b7eca5e58303dc1d8b49bc
Arvandza <arvandza@webmail.umm.ac.id>
2023-01-23 09:43:08 +0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 import tensorflow as tf
 import numpy as np
+<<<<<<< HEAD
 import matplotlib.pyplot as plt
 from werkzeug.utils import secure_filename
+=======
+from werkzeug.utils import secure_filename
+import matplotlib.pyplot as plt
+>>>>>>> 962d838 (update)
 import skimage
 from flask import Flask, render_template, request, send_from_directory
 
@@ -12,10 +17,18 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = "uploads"
 STATIC_FOLDER = "static"
 
+<<<<<<< HEAD
 # Load model
 cnn_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "herbal_leaf_models_vgg16.h5")
 
 
+=======
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
+
+# Load model
+cnn_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "herbal_leaf_models_vgg16.h5")
+
+>>>>>>> 962d838 (update)
 # Preprocess an image
 def classify(model,image):
     class_names = ['Belimbing Wuluh', 'Jambu Biji', 'Jeruk Nipis', 'Kemangi', 'Lidah Buaya', 'Nangka', 'Pandan', 'Pepaya', 'Seledri', 'Sirih']
@@ -40,7 +53,10 @@ def classify(model,image):
         classified.append(classified_prob)
     return labels, classified
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 962d838 (update)
 # home page
 @app.route("/")
 def home():
@@ -54,12 +70,18 @@ def dataset():
 def about():
     return render_template("about.html")
 
+<<<<<<< HEAD
 @app.route("/classify", methods=["POST", "GET"])
 def upload_file():
 
     if request.method == "GET":
         return render_template("home.html")
     else:
+=======
+@app.route("/classify", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == "POST":
+>>>>>>> 962d838 (update)
         try:
             file = request.files["image"]
             upload_image_path = os.path.join(dir_path,'static','uploads', secure_filename(file.filename))
@@ -67,15 +89,25 @@ def upload_file():
             file.save(upload_image_path)
         except FileNotFoundError:
             return render_template("home.html")    
+<<<<<<< HEAD
         label,prob = classify(cnn_model, upload_image_path)
         
+=======
+        label,prob = classify(cnn_model, upload_image_path) 
+    else:
+        return render_template("home.html")
+>>>>>>> 962d838 (update)
     return render_template(
         "class.html", image_file_name=file.filename, label=label, prob=prob
     )
 
 @app.route("/classify/<filename>")
 def send_file(filename):
+<<<<<<< HEAD
     return send_from_directory(UPLOAD_FOLDER, filename)
+=======
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+>>>>>>> 962d838 (update)
 
 if __name__ == "__main__":
     app.debug = True
